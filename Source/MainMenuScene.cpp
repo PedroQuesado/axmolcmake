f194c499db8b24c074899d5567f39836b86445c6
PedroQuesado <51405826+PedroQuesado@users.noreply.github.com>
2024-10-13 20:52:12 +0100

    www

---
+++ Source/MainMenuScene.cpp
@@ -0,0 +1,44 @@
+#include "MainMenuScene.h"
+#include "FairyGUI.h"
+#include "axmol.h"
+
+USING_NS_AX;
+  // Certifique-se de usar o namespace correto para FairyGUI
+
+Scene* MainMenuScene::createScene()
+{
+    auto scene = Scene::create();
+    auto layer = MainMenuScene::create();
+    scene->addChild(layer);
+    return scene;
+}
+
+bool MainMenuScene::init()
+{
+    if (!Scene::init())
+    {
+        return false;
+    }
+
+    // Certifique-se de que o pacote FairyGUI foi carregado corretamente
+    UIPackage::addPackage("MainMenu");
+
+    // Criar o componente principal do menu a partir do pacote
+    menuComponent = UIPackage::createObject("MainMenu", "MainMenuComponent")->as<GComponent>();
+    if (!menuComponent)
+    {
+        AXLOG("Falha ao criar menuComponent");
+        return false;
+    }
+
+  //  auto visibleSize = Director::getInstance()->getVisibleSize();
+   // menuComponent->setPosition(800, 400 );
+
+    GRoot::getInstance()->addChild(menuComponent);
+
+    // Obter referência aos botões e configurar eventos de clique
+  
+    return true;
+}
+
+// Callback para iniciar o jogo
